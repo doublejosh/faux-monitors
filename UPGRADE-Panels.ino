63244215b5f9442053a4e7068a6d7b4778f29de8
doublejosh <joshuaklind@gmail.com>
2018-07-12 04:05:11 -0700

    add second wave screen

--- UPGRADE-Panels.ino
+++ UPGRADE-Panels.ino
@@ -1,5 +1,9 @@
 /**
- * UPGRADE - Faux Computer Monitors.
+ * Faux Computer Monitors panels for UPGRADE vehicle.
+ * 
+ * REMEMBER: To avoid NeoPixel burnout...
+ * add 1000 uF capacitor across power leads
+ * and 470Ω resister on data leads.
  */
 
 #include <Adafruit_NeoPixel.h>
@@ -7,16 +11,18 @@
   #include <avr/power.h>
 #endif
 
-#define PIN 6
-#define SCREEN_SIZE 34
-#define TOTAL_PIX 200
+#define PIN_1 6
+#define SCREEN_SIZE 33
+#define TOTAL_PIX_1 100
 #define BOTTOM_ROW_LENGTH 8
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(TOTAL_PIX, PIN, NEO_GRB + NEO_KHZ800);
+#define PIN_2 7
+#define TOTAL_PIX_2 111
 
-// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across pixel power leads
+Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(TOTAL_PIX_1, PIN_1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(TOTAL_PIX_2, PIN_2, NEO_GRB + NEO_KHZ800);
 
-uint8_t ledShift = 110;
+uint8_t ledShift = 0;
 uint8_t wait = 90;
 
 // Command line.
@@ -29,13 +35,16 @@ uint8_t strengthRange = 10, // devided into 255 brightness.
 uint8_t  changeChanceRow = 15, // out of 100, blinkiness.
          colorListLength = 6;
 uint32_t colorList[6] = {
-           strip.Color(0, 255, 0), // green
-           strip.Color(0, 0, 255), // blue
-           strip.Color(255, 0, 0), // red
-           strip.Color(255, 255, 0), // yellow
-           //strip.Color(255,20, 147), //pink
-           strip.Color(0, 0, 0) // off
+           strip1.Color(255, 255, 0), // yellow
+           strip1.Color(0, 255, 0), // green
+           strip1.Color(0, 0, 255), // blue
+           strip1.Color(255, 0, 0), // red
+           //strip1.Color(255,20, 147), //pink
+           strip1.Color(0, 0, 0) // off
          };
+
+// Wave Line.
+uint8_t waitChanceWave = 50; // out of 100, varied timing.
 
 // Series groups.
 uint8_t  changeChanceSeries = 30, // out of 100, blinkiness.
@@ -50,25 +59,43 @@ void setup() {
 //  // End of trinket special code
   
   randomSeed(analogRead(0));
-  strip.begin();
-  strip.show();
+  strip1.begin();
+  strip2.begin();
+  strip1.show();
+  strip2.show();
 }
 
+/**
+ * Main loop.
+ */
 void loop() {
-  commandLine(ledShift);
-  buttonRow(ledShift + SCREEN_SIZE, BOTTOM_ROW_LENGTH);
-  seriesRows(ledShift + SCREEN_SIZE + BOTTOM_ROW_LENGTH, 3);
-  seriesRows(ledShift + SCREEN_SIZE + BOTTOM_ROW_LENGTH + 3 + 1, 1);
-  seriesRows(ledShift + SCREEN_SIZE + BOTTOM_ROW_LENGTH + 3 + 3, 5);
-  seriesRows(ledShift + SCREEN_SIZE + BOTTOM_ROW_LENGTH + 3 + 3 + 5, 9);
-  seriesRows(ledShift + SCREEN_SIZE + BOTTOM_ROW_LENGTH + 3 + 3 + 5 + 9, 7);
-  seriesRows(ledShift + SCREEN_SIZE + BOTTOM_ROW_LENGTH + 3 + 3 + 5 + 9 + 8, 3);
-  strip.show();
+  // Panel #1.
+  commandLine(0, strip1);
+  buttonRow(SCREEN_SIZE, BOTTOM_ROW_LENGTH);
+  seriesRows(SCREEN_SIZE + BOTTOM_ROW_LENGTH, 3);
+  seriesRows(SCREEN_SIZE + BOTTOM_ROW_LENGTH + 3 + 1, 1);
+  seriesRows(SCREEN_SIZE + BOTTOM_ROW_LENGTH + 3 + 3, 5);
+  seriesRows(SCREEN_SIZE + BOTTOM_ROW_LENGTH + 3 + 3 + 5, 9);
+  seriesRows(SCREEN_SIZE + BOTTOM_ROW_LENGTH + 3 + 3 + 5 + 9, 7);
+  seriesRows(SCREEN_SIZE + BOTTOM_ROW_LENGTH + 3 + 3 + 5 + 9 + 8, 3);
+
+  // Panel #2.
+  waveLine(54, strip2); // Assumed end gap of 3.
+
+  // Panel #3.
+  //heartBeat(strip3);
+
+  strip1.show();
+  strip2.show();
   delay(wait);
 }
 
-void commandLine(uint8_t shift) {
-  uint8_t newStrength, pixelStrength;
+/**
+ * Rolling commands or logging simulation.
+ */
+void commandLine(uint8_t shift, Adafruit_NeoPixel &strip) {
+  uint8_t newStrength,
+          pixelStrength;
 
   // Decide whether to move/adjust, keep scrolling varied.
   if (waitChance > random(1, 100)) {
@@ -89,6 +116,31 @@ void commandLine(uint8_t shift) {
   }
 }
 
+/**
+ * Waveform simulation.
+ */
+void waveLine(uint8_t screenSize, Adafruit_NeoPixel &strip) {
+  uint8_t pixelStrength,
+          pixelIndex;
+  uint32_t color; 
+
+  // Decide whether to move/adjust, keep scrolling varied.
+  if (waitChanceWave > random(1, 100)) {
+    // Loop through screen.
+    for (uint16_t i=0; i<screenSize; i++) {
+      // Determine pixel characteristics.      
+      pixelStrength = random(0, 255),
+      color = (pixelStrength < 170) ? colorWheel(pixelStrength) : 0;
+      // Set screen pixel.
+      strip.setPixelColor(i, color);
+      strip.setPixelColor(((screenSize * 2) + 2) - i, color);
+    }
+  }
+}
+
+/**
+ * Separate pixels animation.
+ */
 void buttonRow(uint8_t start, uint8_t pixelLength) {
   uint32_t color;
 
@@ -96,10 +148,13 @@ void buttonRow(uint8_t start, uint8_t pixelLength) {
   if (changeChanceRow > random(1, 100)) {
     // Set button pixel.
     color = colorList[random(0, colorListLength)];
-    strip.setPixelColor(random(0, pixelLength) + start, color);
+    strip1.setPixelColor(random(0, pixelLength) + start, color);
   }
 }
 
+/**
+ * Grouping of pixel(s) animation.
+ */
 void seriesRows(uint8_t start, uint8_t pixelLength) {
   uint32_t color;
   boolean  already = false;
@@ -110,19 +165,19 @@ void seriesRows(uint8_t start, uint8_t pixelLength) {
 
   // Move existing pixels.
   for (uint8_t i=0; i<pixelLength; i++) {
-    color = strip.getPixelColor(start + i);
+    color = strip1.getPixelColor(start + i);
     // Back to off.
-    strip.setPixelColor(start + i, strip.Color(0, 0, 0));
+    strip1.setPixelColor(start + i, strip1.Color(0, 0, 0));
     
     // Lit pixel.
-    if (color != strip.Color(0, 0, 0)) {
+    if (color != strip1.Color(0, 0, 0)) {
       already = true;
       // Last pixel.
       if (i == (pixelLength - 1)) {
-        //strip.setPixelColor(start, strip.getPixelColor(start + i));
+        //strip1.setPixelColor(start, strip1.getPixelColor(start + i));
       } else {
         // Move pixel along.
-        strip.setPixelColor(start + i + 1, color);
+        strip1.setPixelColor(start + i + 1, color);
         break;
       }
     }
@@ -131,6 +186,22 @@ void seriesRows(uint8_t start, uint8_t pixelLength) {
   // Decide whether to initiate new sequence.
   if (!already && changeChanceSeries > random(1, 100)) {
     // Don't include off.
-    strip.setPixelColor(start, colorList[random(0, colorListLength - 1)]);
+    strip1.setPixelColor(start, colorList[random(0, colorListLength - 1)]);
   }
+}
+
+/**
+ * Input a value 0 to 255 to get a color value.
+ */
+uint32_t colorWheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip1.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip1.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip1.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
